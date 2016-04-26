d91fd1d762d5e046c99f5784cf5ef8724df5b002
Diego <gecko.bona@gmail.com>
2016-04-26 11:00:54 +0000

    Cron module dependency added

--- server.js
+++ server.js
@@ -16,19 +16,19 @@ app.use(methodOverride());
 var router = express.Router();
 
 router.get('/', function(req, res) {
-   res.send('Watchbot is working and doing its magic.');
+    res.send('Watchbot is working and doing its magic.');
 });
 
 app.use(router);  //TODO: Eliminar esta linea
 app.use('/api', router);
 
-app.use(function(req, res, next){
-  res.sendStatus(404);
+app.use(function(req, res, next) {
+    res.sendStatus(404);
 });
 
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.sendStatus(500);
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.sendStatus(500);
 });
 
 app.listen(process.env.PORT);
