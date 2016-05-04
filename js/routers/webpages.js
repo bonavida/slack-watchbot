d8cdef034b3a0e1316140a9184b027935cbd5295
Diego <gecko.bona@gmail.com>
2016-05-04 11:38:38 +0200

    Request dependencied added. Basic API Rest fixed.

--- js/routers/webpages.js
+++ js/routers/webpages.js
@@ -3,35 +3,35 @@ var Webpage  = require('../models/webpage');
 module.exports = function(router) {
 
     router.get('/webpages', function(req, res, next) {
-      Webpage.find(function(err, webpages) {
-          if (!err) {
-              if (!webpages) {
-                  next();
-              } else {
-                  res.json(webpages);
-              }
-          } else {
-              res.send(500, err.message);
-          }
-      });
+        Webpage.find(function(err, webpages) {
+            if (!err) {
+                if (!webpages) {
+                    next();
+                } else {
+                    res.json(webpages);
+                }
+            } else {
+                res.send(500, err.message);
+            }
+        });
     }).post('/webpages', function(req, res, next) {
-      var webpage = new Webpage({
-          name : req.body.name,
-          url : req.body.url,
-          user : req.body.user
-      });
-      webpage.save(function(err, webpage) {
-          if (!err) {
-              if (!webpage) {
-                  next();
-              }
-              res.json(webpage);
-              //res.json({success: true, msg: 'Successful created new user.'});
-          } else {
-              res.send(500, err.message);
-              //return res.json({success: false, msg: 'Username or e-mail already exists.'});
-          }
-      });
+        var webpage = new Webpage({
+            name : req.body.name,
+            url : req.body.url,
+            user : req.body.user
+        });
+        webpage.save(function(err, webpage) {
+            if (!err) {
+                if (!webpage) {
+                    next();
+                }
+                res.json(webpage);
+                //res.json({success: true, msg: 'Successful created new user.'});
+            } else {
+                res.send(500, err.message);
+                //return res.json({success: false, msg: 'Username or e-mail already exists.'});
+            }
+        });
     });
 
 };
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var bodyParser     = require("body-parser");
 var methodOverride = require("method-override");
 var mongoose       = require('mongoose');
 var config         = require('./config/database');
+var request        = require('request');
+var port           = process.env.PORT;
 
 var app = express();
 
@@ -20,7 +22,7 @@ router.get('/', function(req, res) {
     res.send('Watchbot is working and doing its magic.');
 });
 
-//app.use(router);  //TODO: Eliminar esta linea
+app.use(router);  //TODO: Eliminar esta linea
 require('./js/routers/webpages')(router);
 app.use('/api', router);
 
@@ -33,11 +35,28 @@ app.use(function(err, req, res, next) {
     res.sendStatus(500);
 });
 
-mongoose.connect('mongodb://localhost/watchbot', function(err, res) {
+// request('https://eforce.elkno.com/api', function (error, res, body) {
+//     // Website is up
+//     if (!error && res.statusCode === 200) {
+//         console.log("UP");
+//     }
+//
+//     // No error but website not ok
+//     else if (!error) {
+//         console.log("DOWN: " + res.statusCode);
+//     }
+//
+//     // Loading error
+//     else {
+//         console.log("DOWN");
+//     }
+// });
+
+mongoose.connect(config.database, function(err, res) {
     if (err) {
         console.log('ERROR: connecting to Database. ' + err);
     }
-    app.listen(process.env.PORT, function() {
-        console.log("Node server running on http://localhost:" + process.env.PORT);
+    app.listen(port, function() {
+        console.log("Watchbot Node server running on http://localhost:" + port);
     });
 });
