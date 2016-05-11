80ed276ebf94874c1f73a73cb475dfd9a148754d
Diego <al095660@uji.es>
2016-05-11 11:58:53 +0200

    Added some comments in the cron configuration

--- js/modules/cron-watch.js
+++ js/modules/cron-watch.js
@@ -8,22 +8,35 @@ dotenv.load();
 /** Carga la API de Slack con la URL asociada al Incoming Webhooks de la cuenta de Slack del equipo */
 var slack = new slackAPI(process.env.WEBHOOK);
 
-
-var cronJob = cron.job("*/10 * * * * *", function(){
+/** Configura la tarea para que, de forma periódica, realice llamadas a una
+  * lista de páginas web para saber si están caídas. Si alguna lo está,
+  * envía un mensaje a Slack */
+var cronJob = cron.job("*/10 * * * * *", function() {
     // perform operation e.g. GET request http.get() etc.
+    //TODO
     slack.send({text:"Hello"});
 });
 
-/** Métodos */
+
+/** MÉTODOS */
+
+/** Método para iniciar la tarea */
 var cronStart = function() {
   cronJob.start();
 };
 
+/** Método para parar la tarea */
 var cronStop = function() {
   cronJob.stop();
 };
 
+/** Método para reiniciar la tarea **/
+var cronRestart = function() {
+  //TODO
+};
+
 module.exports = {
   start: cronStart,
-  stop: cronStop
+  stop: cronStop,
+  restart: cronRestart
 };
