3393a1cb1339f50e3aa294d06ae416567548252e
Diego <al095660@uji.es>
2016-05-16 11:30:59 +0200

    Webpage model extended. Removed the refactoring in the response message.

--- js/models/webpage.js
+++ js/models/webpage.js
@@ -6,18 +6,26 @@ var Schema = mongoose.Schema;
  *      url: URL de la página web
  *      dateAdded: Fecha en la que se añadió
  *      user: Usuario de Slack que la añadió
- *      incidencies: Nº de incidencias (veces que una página web se ha caído) con sus respectivas fechas
+ *      channel: Canal de Slack desde el que se añadió
+ *      timeout: Intervalo (en minutos) para hacer los pings
+ *      averageResponseTime: Tiempo medio de respuesta de la página web
+ *      numPings: Nº de pings realizados a la página web
+ *      numIncidencies: Nº de veces que una página web se ha caído
+ *      lastIncidency: Fecha de la última vez que el server se ha caído
  */
 var webpageSchema = new Schema({
     name : { type: String, unique: true },
     url : { type: String, unique: true },
     dateAdded : Date,
     user : String,
-    channel: String,
-    incidencies : [{
-        incidencyDate : Date
-    }]
+    channel : String,
+    timeout : { type: Number, default: 15 },
+    averageResponseTime : { type: Number, default: 0 },
+    numPings : { type: Number, default: 0 },
+    numIncidencies : { type: Number, default: 0 },
+    lastIncidency : Date
 });
+
 
 /** Cada vez que se crea una nueva webpage automáticamente se añadirá la fecha actual */
 webpageSchema.pre('save', function(next) {
@@ -33,5 +41,6 @@ webpageSchema.pre('save', function(next) {
 
     next();
 });
+
 
 module.exports = mongoose.model('WebPage', webpageSchema);
--- js/modules/slash-commands.js
+++ js/modules/slash-commands.js
@@ -37,36 +37,47 @@ var data = function(req, res) {
     // }
 
     var message = {
-        response_type: "ephemeral"
+        response_type: "ephemeral",
+        text: "",
+        attachments: []
     };
 
     switch (option) {
 
         case "help":
-            message.text = "Esto es una línea.\nY ésta es otra.";
+            res.json({
+                response_type: "ephemeral",
+                text:"Esto es una línea.\nY ésta es otra."
+            });
             break;
 
         case "add":
             /** Comprueba si el número de parámetros es correcto */
             if (text.length < 2) {
-                message.text = "Error al añadir la página web.";
-                message.attachments = [{
-                    text: "Número de parámetros incorrecto.\n/watch add <nombre> <url>",
-                    color: "danger"
-                }];
+                res.json({
+                    response_type: "ephemeral",
+                    text: "Error al añadir la página web.",
+                    attachments: [{
+                        text: "Número de parámetros incorrecto.\n/watch add <nombre> <url>",
+                        color: "danger"
+                    }]
+                });
             } else {
 
                 var url = text.pop(); // La URL es el último parámetro
 
                 /** Comprueba si la URL de la página web es válida a través de una expresión regular */
                 if (!url.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/)) {
-                    message.text = "Error al añadir la página web.";
-                    message.attachments = [{
-                        text: "La URL de la página web ha de tener un formato válido.\n" +
-                              "http://ejemplo.com\nhttp://wwww.ejemplo.com\n" +
-                              "http://ejemplo.com/ejemplo.html?q=ejemplo\nhttps://ejemplo.com",
-                        color: "danger"
-                    }];
+                    res.json({
+                        response_type: "ephemeral",
+                        text: "Error al añadir la página web.",
+                        attachments: [{
+                            text: "La URL de la página web ha de tener un formato válido.\n" +
+                                  "http://ejemplo.com\nhttp://wwww.ejemplo.com\n" +
+                                  "http://ejemplo.com/ejemplo.html?q=ejemplo\nhttps://ejemplo.com",
+                            color: "danger"
+                        }]
+                    });
                 } else {
 
                     var name = text.join(" "); // Une las cadenas con un espacio en blanco para formar el nombre de la página web
@@ -81,18 +92,23 @@ var data = function(req, res) {
                     /** Guarda los datos en la base de datos */
                     WebpageService.add(webpage, function(err, msg) {
                         if (err) {
-                            message.text = "Error al añadir la página web.";
-                            message.attachments = [{
-                                text: msg,
-                                color: "danger"
-                            }];
+                            return res.json({
+                                response_type: "ephemeral",
+                                text: "Error al añadir la página web.",
+                                attachments: [{
+                                    text: msg,
+                                    color: "danger"
+                                }]
+                            });
                         } else {
-                            message.response_type = "in_channel";
-                            message.text = msg;
-                            message.attachments = [{
-                                text: webpage.name + "\n" + webpage.url,
-                                color: "good"
-                            }];
+                            res.json({
+                                response_type: "in_channel",
+                                text: msg,
+                                attachments: [{
+                                    text: webpage.name + "\n" + webpage.url,
+                                    color: "good"
+                                }]
+                            });
                         }
                     });
                 }
@@ -103,26 +119,34 @@ var data = function(req, res) {
         case "remove":
             /** Comprueba si el número de parámetros es correcto */
             if (text.length === 0) {
-                message.text = "Error al eliminar la página web.";
-                message.attachments = [{
-                    text: "Número de parámetros incorrecto.\n/watch remove <nombre>",
-                    color: "danger"
-                }];
+                res.json({
+                    response_type: "ephemeral",
+                    text: "Error al eliminar la página web.",
+                    attachments: [{
+                        text: "Número de parámetros incorrecto.\n/watch remove <nombre>",
+                        color: "danger"
+                    }]
+                });
             } else {
 
                 var removeName = text.join(" "); // Nombre de la página web a borrar
 
-                /** Elimina la página web la base de datos */
+                /** Elimina la página web de la base de datos */
                 WebpageService.remove(removeName, function(removed, msg) {
                     if (!removed) {
-                        message.text = "Error al eliminar la página web.";
-                        message.attachments = [{
-                            text: msg,
-                            color: "danger"
-                        }];
+                        return res.json({
+                            response_type: "ephemeral",
+                            text: "Error al eliminar la página web.",
+                            attachments: [{
+                                text: msg,
+                                color: "danger"
+                            }]
+                        });
                     } else {
-                        message.response_type = "in_channel";
-                        message.text = msg;
+                        res.json({
+                            response_type: "in_channel",
+                            text: msg
+                        });
                     }
                 });
             }
@@ -130,43 +154,57 @@ var data = function(req, res) {
             break;
 
         case "list":
-            /** Comprueba si el número de parámetros es correcto */
-            if (text.length > 2) {
-                message.text = "Error al listar las páginas web.";
-                message.attachments = [{
-                    text: "Número de parámetros incorrecto.\n/watch list\n/watch list all",
-                    color: "danger"
-                }];
+
+            if (text.length > 2) {  // Comprueba si el número de parámetros es correcto
+                res.json({
+                    response_type: "ephemeral",
+                    text: "Error al listar las páginas web.",
+                    attachments: [{
+                        text: "Número de parámetros incorrecto.\n/watch list\n/watch list all",
+                        color: "danger"
+                    }]
+                });
             } else if (text.length === 1) {  // Lista todas las páginas web
                 if (text[0] === "all") {  // Comando válido
                     WebpageService.getAllWebpages(function (err, webpages) {
                         if (err) {
-                            message.text = "Error al listar las páginas web.";
-                            message.attachments = [{
-                                text: "Ha habido un error. Inténtelo de nuevo.",
-                                color: "danger"
-                            }];
+                            return res.json({
+                                response_type: "ephemeral",
+                                text: "Error al listar las páginas web.",
+                                attachments: [{
+                                    text: "Ha habido un error. Inténtelo de nuevo.",
+                                    color: "danger"
+                                }]
+                            });
                         } else {
                             if (webpages.length === 0) {
-                                message.text = "No se ha registrado ninguna página web.";
+                                return res.json({
+                                    response_type: "ephemeral",
+                                    text: "No se ha registrado ninguna página web."
+                                });
                             } else {
-                                message.response_type = "in_channel";
-                                message.text = "Se han registrado las siguientes páginas web:";
-                                message.attachments = [{
-                                    text: listToString(webpages, true),
-                                    color: "0080ff",
-                                    mrkdwn_in: ["text"]
-                                }];
+                                res.json({
+                                    response_type: "in_channel",
+                                    text: "Se han registrado las siguientes páginas web:",
+                                    attachments: [{
+                                        text: listToString(webpages, true, false),
+                                        color: "0080ff",
+                                        mrkdwn_in: ["text"]
+                                    }]
+                                });
                             }
                         }
                     });
                 } else {  // Comando no válido
-                    message.text = "Error al listar las páginas web.";
-                    message.attachments = [{
-                        text: "Comando no identificado. Por favor, usa los siguientes comandos:\n" +
-                              "/watch list\n/watch list all",
-                        color: "danger"
-                    }];
+                    res.json({
+                        response_type: "ephemeral",
+                        text: "Error al listar las páginas web.",
+                        attachments: [{
+                            text: "Comando no identificado. Por favor, usa los siguientes comandos:\n" +
+                                  "/watch list\n/watch list all",
+                            color: "danger"
+                        }]
+                    });
                 }
 
             } else {  // Lista las páginas web registradas por el usuario que escribe el comando
@@ -175,22 +213,30 @@ var data = function(req, res) {
 
                 WebpageService.getWebpages(userName, function(err, webpages) {
                     if (err) {
-                        message.text = "Error al listar las páginas web.";
-                        message.attachments = [{
-                            text: "Ha habido un error. Inténtelo de nuevo.",
-                            color: "danger"
-                        }];
+                        return res.json({
+                            response_type: "ephemeral",
+                            text: "Error al listar las páginas web.",
+                            attachments: [{
+                                text: "Ha habido un error. Inténtelo de nuevo.",
+                                color: "danger"
+                            }]
+                        });
                     } else {
                         if (webpages.length === 0) {
-                            message.text = "Este usuario no ha registrado ninguna página web.";
+                            return res.json({
+                                response_type: "ephemeral",
+                                text: "No has registrado ninguna página web."
+                            });
                         } else {
-                            message.response_type = "in_channel";
-                            message.text = "El usuario @" + userName + " ha registrado las siguientes páginas web:";
-                            message.attachments = [{
-                                text: listToString(webpages, false),
-                                color: "0080ff",
-                                mrkdwn_in: ["text"]
-                            }];
+                            res.json({
+                                response_type: "in_channel",
+                                text: "El usuario @" + userName + " ha registrado las siguientes páginas web:",
+                                attachments: [{
+                                    text: listToString(webpages, false, false),
+                                    color: "0080ff",
+                                    mrkdwn_in: ["text"]
+                                }]
+                            });
                         }
                     }
                 });
@@ -199,33 +245,58 @@ var data = function(req, res) {
             break;
 
         default:
-            message.text = "Comando no identificado.\nEscribe /watch help para más información.";
+            res.json({
+                response_type: "ephemeral",
+                text:"Comando no identificado.\nEscribe /watch help para más información."
+            });
 
     }
-
-    res.json(message);
 
     //TODO res.end();
 
 };
 
-function listToString(webpages, listAll) {
+
+/** Método privado que convierte una lista de páginas web en una cadena de texto
+ *  - Si isAll es true, es que se han de mostrar todas las páginas web
+ *  - Si isLog es true, es que se han de mostrar más detalles
+ */
+function listToString(webpages, isAll, isLog) {
     var msg = "";
     for (var i = 0; i < webpages.length; i++) {
         var date = new Date(webpages[i].dateAdded);
-        var dayAdded = date.getDate();
-        var monthAdded = date.getMonth() + 1;  // El mes va del 0 al 11
-        var yearAdded = date.getFullYear();
-        var hourAdded = date.getHours() + ":" + date.getMinutes();
-        var incidencies = webpages[i].incidencies===undefined ? 0 : webpages[i].incidencies.length;
+        var strDate = formattedDate(date);
         msg += "*" + webpages[i].name + "*" +
                "  " + webpages[i].url + "\n" +
-               "Añadido el " + dayAdded + "/" + monthAdded + "/" + yearAdded + " a las " + hourAdded;
-        if (listAll) {
-            msg += " por el usuario @" + webpages[i].user;
+               "Añadido el " + strDate;
+        if (isAll) {
+            msg += " por el usuario @" + webpages[i].user + "\n";
+        } else {
+            msg += "\n";
         }
-        msg += "\nNº de veces caído: " + incidencies + "\n\n";
+        msg += "Nº de veces caído: " + webpages[i].numIncidencies + "\n";
+        if (isLog) {
+            if (webpages[i].numIncidencies !== 0) {
+                var lastIncidency = new Date(webpages[i].lastIncidency);
+                var strLastIncidency = formattedDate(lastIncidency);
+                msg += "Última vez caído el " + strLastIncidency;
+            }
+            msg += "Tiempo de respuesta medio: " + webpages[i].averageResponseTime + "\n";
+        }
+        msg += "\n";
     }
+    return msg;
+}
+
+/** Método privado que convierte una fecha en una cadena de texto */
+function formattedDate(date) {
+    var dayAdded = date.getDate();
+    var monthAdded = date.getMonth() + 1;  // El mes va del 0 al 11
+    var yearAdded = date.getFullYear();
+    var hourAdded = date.getHours() + ":" + date.getMinutes();
+
+    var msg = dayAdded + "/" + monthAdded + "/" + yearAdded + " a las " + hourAdded;
+
     return msg;
 }
 
